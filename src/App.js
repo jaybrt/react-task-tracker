546c2211487d19ea3a93462046c3d435bd19ea10
Jay Landers <jay@jaybirdgroup.net>
2021-02-23 23:27:23 -0700

    Added task UI, buton to remove tasks, and toggling reminder value

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
 import Header from './components/Header'
+import Tasks from './components/Tasks'
+import { useState } from 'react'
 
 const App = () => {
+
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Dr. Appointment',
+      day: 'Feb 5th at 2:30pm',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Meeting at School',
+      day: 'Feb 6th at 1:30pm',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'Food Shopping',
+      day: 'Feb 5th at 2:30pm',
+      reminder: false,
+    }
+  ])
+
+  //Delete Tasks
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  //Toggle reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
+  }
+
   return (
-    <div className="App">
+    <div className="container">
      <Header />
+     {tasks.length > 0 ? ( <Tasks tasks={tasks} onDelete={deleteTask}
+      onToggle={toggleReminder}/>) : 'You Have Completed Everything'}
     </div>
   );
 }
