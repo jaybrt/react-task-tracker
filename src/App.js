ff7bdb50e977a9b95cacd6d6aaed7f37a5bb2a3d
Jay Landers <jay@jaybirdgroup.net>
2021-03-03 00:05:22 -0700

    added routing with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Route} from 'react-router-dom'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
-import { useState, useEffect } from 'react'
+import Footer from './components/Footer'
+import About from './components/About'
 
 const App = () => {
 
@@ -85,12 +88,20 @@ const App = () => {
   }
 
   return (
-    <div className="container">
-     <Header onClick={toggleForm} formOpen={showAddTask}/>
-     {showAddTask && <AddTask onAdd={addTask}/>}
-     {tasks.length > 0 ? ( <Tasks tasks={tasks} onDelete={deleteTask}
-      onToggle={toggleReminder}/>) : 'You Have Completed Everything'}
-    </div>
+    <Router>
+      <div className="container">
+       <Header onClick={toggleForm} formOpen={showAddTask}/>
+        <Route path='/' exact render={(props) => (
+          <>
+          {showAddTask && <AddTask onAdd={addTask}/>}
+          {tasks.length > 0 ? ( <Tasks tasks={tasks} onDelete={deleteTask}
+           onToggle={toggleReminder}/>) : 'You Have Completed Everything'}
+          </>
+        )} />
+        <Route path='/about' component={About} />
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,15 @@
+import { useLocation } from 'react-router-dom'
 import Button from './Button'
 import PropTypes from 'prop-types'
 
 const Header = ({ title, onClick, formOpen }) => {
+  const location = useLocation()
 
   return(
     <header className='header'>
       <h1>{title}</h1>
-      <Button color={formOpen ? 'red' : 'green'}
-      text={formOpen ? 'Done' : 'Add'} onClick={onClick}/>
+      {location.pathname === '/' && <Button color={formOpen ? 'red' : 'green'}
+      text={formOpen ? 'Done' : 'Add'} onClick={onClick}/>}
     </header>
   )
 }
